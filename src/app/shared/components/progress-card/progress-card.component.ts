94a61c6709848224c9ae93fa28379bae560d2de3
Your Name <you@example.com>
2018-09-09 21:25:28 +0530

    progress card

--- src/app/shared/components/progress-card/progress-card.component.ts
+++ src/app/shared/components/progress-card/progress-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress-card',
   templateUrl: './progress-card.component.html',
-  styleUrls: ['./progress-card.component.css']
+  styleUrls: ['./progress-card.component.scss']
 })
 export class ProgressCardComponent implements OnInit {
 
